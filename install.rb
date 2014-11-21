fb9cbb48cb5f4351812dab43466b3e1513a99faf
Adam Sharp <adsharp@me.com>
2014-11-21 14:11:14 +1100

    install.rb: Make sure .vimbackup and .vimundo exist

--- install.rb
+++ install.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/ruby
 
+# Homebrew
+
 system "sed <Brewfile 's/^/brew /' | sh"
+
+# rcm
 
 rcrc = File.read(File.expand_path("../rcrc", __FILE__)).chomp
 excludes = rcrc.gsub(/^EXCLUDES="(.+)"$/, '\1')
 rcup_opts = excludes.split(' ').map {|e| "-x #{e}"}.join(' ')
 
 system "rcup #{rcup_opts}"
+
+# vim backup & undo
+
+system "mkdir -p $HOME/.vim{backup,undo}"
