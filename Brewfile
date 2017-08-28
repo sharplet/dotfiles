0eb427a2ed8cb82d883ec6c10a39c5c6dfd42ba2
Adam Sharp <adam@sharplet.me>
2017-08-28 14:23:38 -0400

    Define a swift package for compiled commands

--- Brewfile
+++ Brewfile
@@ -2,4 +2,4 @@ update
 tap thoughtbot/formulae
 tap sharplet/formulae
 tap kylef/formulae
-install hub vim rcm pick chisel icdiff git ag todogrep swiftenv
+install hub vim rcm pick chisel icdiff git ag todogrep swiftenv jq
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+list_swift_executables() {
+  swift package describe --type json \
+    | jq --raw-output '.modules | map(select(type == "executable")) | .[].name'
+}
+
+swift build -c release
+list_swift_executables | xargs -I{} ln -sf ../.build/release/{} scripts/{}
