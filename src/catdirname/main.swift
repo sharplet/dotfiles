72dd83bfbb993185f774fac0a113723109ca2d65
Adam Sharp <adam@sharplet.me>
2018-07-19 11:15:25 -0400

    functions: Improve performance of go()
    Removing some extraneous operators from the `find` invocation seems to
    have sped things up nicely.

--- src/catdirname/main.swift
+++ src/catdirname/main.swift
@@ -2,8 +2,12 @@
 
 import Darwin.C
 
+func ascii(_ scalar: Unicode.Scalar) -> Int32 {
+  return Int32(UInt8(ascii: scalar))
+}
+
 let args = CommandLine.arguments.dropFirst()
-let terminator = Int32(args.contains("-0") ? UInt8(ascii: "\0") : UInt8(ascii: "\n"))
+let terminator = args.contains("-0") ? ascii("\0") : ascii("\n")
 
 while let path = readLine() {
   let dir = path.withCString { dirname(UnsafeMutablePointer(mutating: $0)) }
