c1e3dd3e21d8403a6c24e4880d4893a03b4af271
Adam Sharp <adam@sharplet.me>
2018-10-05 17:19:22 +1000

    Update Package.swift to tools version 4.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
-  name: "sharplet-dotfiles"
+  name: "sharplet-dotfiles",
+  targets: [
+    .target(name: "catdirname"),
+  ]
 )
