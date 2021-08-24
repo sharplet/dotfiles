ba2e52cb17156fda9fd6c939a1d63e4e77470cfd
Adam Sharp <adam@sharplet.me>
2021-08-24 14:46:52 -0400

    swiftpm: Update to tools version 5.5 and add missing dependencies

--- Package.swift
+++ Package.swift
@@ -1,20 +1,32 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
   name: "sharplet-dotfiles",
   platforms: [
-    .macOS(.v10_15),
+    .macOS(.v12),
   ],
   dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.4"),
     .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.0"),
   ],
   targets: [
-    .target(name: "catdirname"),
-    .target(name: "git-prlog", dependencies: ["Util"]),
-    .target(name: "git-sync", dependencies: ["Util"]),
-    .target(name: "ls-projects", dependencies: ["Regex", "Util"]),
+    .executableTarget(name: "catdirname"),
+    .executableTarget(name: "focus", dependencies: [.argumentParser]),
+    .executableTarget(name: "git-prlog", dependencies: ["Util"]),
+    .executableTarget(name: "git-sync", dependencies: ["Util"]),
+    .executableTarget(name: "ls-projects", dependencies: [.regex, "Util"]),
     .target(name: "Util"),
   ]
 )
+
+extension Target.Dependency {
+  static var argumentParser: Target.Dependency {
+    .product(name: "ArgumentParser", package: "swift-argument-parser")
+  }
+
+  static var regex: Target.Dependency {
+    "Regex"
+  }
+}
