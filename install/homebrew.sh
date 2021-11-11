b3d2f085385f8d7e2dafaa57bf12d66ee333659e
Adam Sharp <sharplet@apple.com>
2021-11-11 11:09:02 -0500

    install/homebrew.sh: only install public Homebrew

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -1,29 +1,10 @@
-internal_homebrew_url="https://$GITHUB_INTERNAL/raw/gist/sharplet/4d2596f12746cd6e72c156d199b7df7d/raw/b05a90046acf3268b81947e6bfcd8d5f2414baf0/install_homebrew.sh"
+install_homebrew() {
+  set -e
 
-if ! iscmd brew \
-  && install_homebrew_tmp="$(mktemp -d /tmp/install_homebrew.XXXXXX)" \
-  && /usr/bin/curl -fsSL -o "$install_homebrew_tmp/install_homebrew.sh" "$internal_homebrew_url"
-then
-  source "$install_homebrew_tmp/install_homebrew.sh"
-  rm -rf "$install_homebrew_tmp"
-  unset install_homebrew_tmp
-else
-  install_homebrew() {
-    iscmd brew && return
+  iscmd brew && echo "Homebrew already installed." || (
+    homebrew_url=https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
-    [ -n "$ZSH_VERSION" ] && read -q "install_public?Install public Homebrew?" \
-      || read -p "Install public Homebrew?" install_public
-
-    case "$install_public" in
-      [Yy]*)
-        /bin/bash -c "$(/usr/bin/curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh")"
-        ;;
-      *)
-        echo >&2 "fatal: Unable to install Homebrew"
-        return 1
-        ;;
-    esac
-  }
-fi
-
-unset internal_homebrew_url
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(/usr/bin/curl -fsSL "$homebrew_url")"
+  )
+}
